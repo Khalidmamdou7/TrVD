f05dbfcf8157c38a764a0c77b60ac96af3a1824f
Khaled Mamdouh <khalidmamdou7@gmail.com>
2024-03-16 16:40:58 +0200

    add required file to parition the whole data

---
+++ partition_data.py
@@ -0,0 +1,24 @@
+import pickle
+from sklearn.model_selection import train_test_split
+
+# Load dataset
+with open("./dataset/dataset.pkl", "rb") as f:
+    dataset = pickle.load(f)
+
+# Shuffle dataset
+# Assuming dataset is a list or a pandas DataFrame
+# Shuffle if necessary, if not already shuffled
+
+# Split dataset
+train_data, temp_data = train_test_split(dataset, test_size=0.2, random_state=42)
+val_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42)
+
+# Save splits
+with open("trvd_train.pkl", "wb") as f:
+    pickle.dump(train_data, f)
+
+with open("trvd_val.pkl", "wb") as f:
+    pickle.dump(val_data, f)
+
+with open("trvd_test.pkl", "wb") as f:
+    pickle.dump(test_data, f)
