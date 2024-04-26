bd173c2e90b9ada6acdf8edad45f84a39f89049d
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2024-04-26 05:09:55 +0300

    Update normalization.py

--- normalization.py
+++ normalization.py
@@ -3,18 +3,19 @@ import re
 from clean_gadget import clean_gadget
 import os
 
+
 def normalization(source):
     nor_code = []
-    for fun in source['code']:
-        lines = fun.split('\n')
+    for fun in source["code"]:
+        lines = fun.split("\n")
         # print(lines)
-        code = ''
+        code = ""
         for line in lines:
             line = line.strip()
-            line = re.sub('//.*', '', line)
-            code += line + ' '
+            line = re.sub("//.*", "", line)
+            code += line + " "
         # code = re.sub('(?<!:)\\/\\/.*|\\/\\*(\\s|.)*?\\*\\/', "", code)
-        code = re.sub('/\\*.*?\\*/', '', code)
+        code = re.sub("/\\*.*?\\*/", "", code)
         code = clean_gadget([code])
         nor_code.append(code[0])
         # print(code[0])
@@ -22,23 +23,23 @@ def normalization(source):
 
 
 def mutrvd():
-    train = pd.read_pickle('./dataset/trvd_train.pkl')
-    test = pd.read_pickle('./dataset/trvd_test.pkl')
-    val = pd.read_pickle('./dataset/trvd_val.pkl')
+    train = pd.read_pickle("./dataset/trvd_train.pkl")[
+        0
+    ]  # Assuming the first element of the tuple is the DataFrame
+    test = pd.read_pickle("./dataset/trvd_test.pkl")[0]
+    val = pd.read_pickle("./dataset/trvd_val.pkl")[0]
 
-    train['code'] = normalization(train)
-    if not os.path.exists('./dataset/mutrvd'):
-        os.makedirs('./dataset/mutrvd')
-    train.to_pickle('./dataset/mutrvd/train.pkl')
+    train["code"] = normalization(train)
+    if not os.path.exists("./dataset/mutrvd"):
+        os.makedirs("./dataset/mutrvd")
+    train.to_pickle("./dataset/mutrvd/train.pkl")
 
-    test['code'] = normalization(test)
-    test.to_pickle('./dataset/mutrvd/test.pkl')
+    test["code"] = normalization(test)
+    test.to_pickle("./dataset/mutrvd/test.pkl")
 
-    val['code'] = normalization(val)
-    val.to_pickle('./dataset/mutrvd/val.pkl')
+    val["code"] = normalization(val)
+    val.to_pickle("./dataset/mutrvd/val.pkl")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     mutrvd()
-
-
